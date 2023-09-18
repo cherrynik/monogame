74f145ac439c760f81725a1f57f85bf995169064
Nik Cherednik <ighosta9@gmail.com>
2023-09-18 10:12:58 +0300

    refactor: put container in try

--- src/Apps/GameDesktop/Program.cs
+++ src/Apps/GameDesktop/Program.cs
@@ -7,15 +7,17 @@ using Serilog.Core;
 IConfigurationRoot configuration = ConfigurationFactory.Create();
 using Logger logger = LogFactory.Create(configuration);
 
-ContainerOptions containerOptions =
-    new ContainerOptions
-    {
-        LogFactory = _ => entry => logger.ForContext<ServiceContainer>().Verbose($"{entry.Message}")
-    };
-using ServiceContainer container = new(containerOptions);
+logger.ForContext<Program>().Verbose("Configuration & Logger (+ Sentry) initialized");
 
 try
 {
+    ContainerOptions containerOptions =
+        new ContainerOptions
+        {
+            LogFactory = _ => entry => logger.ForContext<ServiceContainer>().Verbose($"{entry.Message}")
+        };
+    using ServiceContainer container = new(containerOptions);
+
     container.Register(_ => configuration, new PerContainerLifetime());
     container.Register(_ => logger, new PerContainerLifetime());
 
